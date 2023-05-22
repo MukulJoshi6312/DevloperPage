b5d6f68467dda751049c2697bea1558781352aea
Mukul joshi <80614782+MukulJoshi6312@users.noreply.github.com>
2023-05-22 16:35:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,47 @@ topScrollButton.addEventListener("click", () => {
     // document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 })
+
+
+
+// Login from
+const formOpenBtn = document.querySelector('.loginBtn');
+const loginsignup = document.querySelector(".loginsignup");
+const closeBtn = document.querySelector('.form_close')
+const formContainer = document.querySelector(".form_container");
+const signupBtn = document.querySelector("#signup");
+const pwShowHide = document.querySelectorAll('.pw_hide');
+const loginBtn = document.querySelector('#login');
+formOpenBtn.addEventListener('click', () => {
+    navbar.classList.remove('show');
+    loginsignup.classList.add("show")
+
+})
+
+
+
+closeBtn.addEventListener('click', () => loginsignup.classList.remove("show"))
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active")
+})
+
+
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active")
+})
+
+pwShowHide.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        if (getPwInput.type === "password") {
+            getPwInput.type = "text";
+            icon.classList.replace("uil-eye-slash", "uil-eye")
+        } else {
+            getPwInput.type = "password";
+            icon.classList.replace("uil-eye", "uil-eye-slash")
+        }
+    })
+})
